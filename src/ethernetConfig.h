65d808e2b6cfc38ec095c6805dccaeddde16bc1f
Cutler, Philip <greenthegarden@gmail.com>
2022-01-03 22:27:35 +1030

    updated mac address

--- src/ethernetConfig.h
+++ src/ethernetConfig.h
@@ -10,7 +10,7 @@
 
 #if ETHERNET_SHIELD_VERSION == 1
 #include <Ethernet.h>
-byte mac[] = { 0xDD, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEA};
 #elif ETHERNET_SHIELD_VERSION == 2
 #include <Ethernet2.h>
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0xFC, 0xA9 };
@@ -19,6 +19,30 @@ byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0xFC, 0xA9 };
 EthernetClient ethernetClient;
 
 const unsigned long NETWORK_STARTUP_DELAY = 1500UL;
+
+void print_ip() {
+  for (byte thisByte = 0; thisByte < 4; thisByte++)
+  { // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  } //end for
+  Serial.println();
+}
+
+void print_mac() {
+  byte macBuffer[6];              // create a buffer to hold the MAC address
+  Ethernet.MACAddress(macBuffer); // fill the buffer
+  Serial.print("The MAC address is: ");
+  for (byte octet = 0; octet < 6; octet++)
+  {
+    Serial.print(macBuffer[octet], HEX);
+    if (octet < 5)
+    {
+      Serial.print('-');
+    }
+  }
+  Serial.println();
+}
 
 void ethernet_init()
 {
@@ -32,13 +56,9 @@ void ethernet_init()
   else {
     Serial.print("DHCP IP: ");
   }
-  for (byte thisByte = 0; thisByte < 4; thisByte++)
-  { // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print(".");
-  } //end for
-  Serial.println();
-
+  print_ip();
+  print_mac();
+  
   // give the Ethernet shield a second to initialize
   delay(NETWORK_STARTUP_DELAY);
 }
